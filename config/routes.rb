075123383c178e2f18832edc5c6ef94add48f402
Niall Mahford <niall.mahford@gmail.com>
2017-11-22 00:23:14 -0800

    Created Api::Users Controller, Filled out Application Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'users/new'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
