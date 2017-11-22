b83f759d3ad8df5b52ac77f19928b4fa198689ca
Niall Mahford <niall.mahford@gmail.com>
2017-11-22 00:33:48 -0800

    Created session controller, modified routes for session and users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users/new'
-  end
 
-  namespace :api do
-    get 'users/create'
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:new, :create, :destroy]
+    resources :users, only: [:new, :create]
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
