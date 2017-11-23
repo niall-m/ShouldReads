4b5667412bfb3a26edc3bde6d1ba982b503a9359
Niall Mahford <niall.mahford@gmail.com>
2017-11-22 17:55:19 -0800

    minor bug fix for entry file

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -5,7 +5,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const Root = ({ store }) => (
-    <Provider>
+    <Provider store={store}>
        <HashRouter>
             <App />
         </HashRouter>
--- frontend/should_reads.jsx
+++ frontend/should_reads.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -7,5 +8,5 @@ document.addEventListener("DOMContentLoaded", () => {
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to ShouldReads</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
