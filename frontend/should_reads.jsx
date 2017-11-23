f0a9132a4313887533689adf9909ac05cc545746
Niall Mahford <niall.mahford@gmail.com>
2017-11-22 17:30:05 -0800

    Imported thunk, logger, configured store, put on entry window for testing.

--- frontend/should_reads.jsx
+++ frontend/should_reads.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
-// import configureStore from './store/store';
-
-import { signup, login, logout } from './util/session_api_util';
-
-window.signup = signup;
-window.login = login;
-window.logout = logout;
-
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-    // const store = configureStore();
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to ShouldReads</h1>, root);
 });
