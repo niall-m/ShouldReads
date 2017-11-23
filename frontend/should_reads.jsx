df4ccf710f6d40add530d2503c28c3dc2baa0fe4
Niall Mahford <niall.mahford@gmail.com>
2017-11-22 16:45:27 -0800

    Session API Util, test methods on window.

--- frontend/should_reads.jsx
+++ frontend/should_reads.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 // import Root from './components/root';
 // import configureStore from './store/store';
 
+import { signup, login, logout } from './util/session_api_util';
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
+
+
 document.addEventListener("DOMContentLoaded", () => {
     // const store = configureStore();
     const root = document.getElementById("root");
