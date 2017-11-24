bc0cb46cc13c4087103c3a907f480ee2acc3650b
Niall Mahford <niall.mahford@gmail.com>
2017-11-24 15:31:21 -0800

    removed greetingscontainer.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import GreetingContainer from './greeting/greeting_container';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
