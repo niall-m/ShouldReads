bedc61ab1fb530d12cc63e92b95aee706fd948b9
Niall Mahford <niall.mahford@gmail.com>
2017-11-24 14:46:04 -0800

    Created Auth and Protected routes.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,13 +5,17 @@ import GreetingContainer from './greeting/greeting_container';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
+import { 
+    AuthRoute,
+    ProtectedRoute
+} from '../util/route_utils';
 
 const App = () => (
     <div>
         <header>
             <Route path="/" component={NavBarContainer} />
-            <Route path="/signup" component={SignupContainer} />
-            <Route path="/login" component={LoginContainer} />
+            <AuthRoute path="/signup" component={SignupContainer} />
+            <AuthRoute path="/login" component={LoginContainer} />
         </header>
     </div>
 );
