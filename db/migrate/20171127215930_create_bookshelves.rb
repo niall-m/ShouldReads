698286136bc468fdd98f34ce26418517e7a59783
Niall Mahford <niall.mahford@gmail.com>
2017-11-27 14:02:45 -0800

    Create bookshelf model

---
+++ db/migrate/20171127215930_create_bookshelves.rb
@@ -0,0 +1,11 @@
+class CreateBookshelves < ActiveRecord::Migration[5.1]
+  def change
+    create_table :bookshelves do |t|
+      t.integer :user_id, null: false
+      t.string :shelf_name, null: false
+
+      t.timestamps
+    end
+    add_index :bookshelves, :user_id
+  end
+end
