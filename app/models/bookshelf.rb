5f95d801e69c57c8ec0588ccb03650baa6cf2994
Niall Mahford <niall.mahford@gmail.com>
2017-11-27 15:07:53 -0800

    Generate, validate and migrate bookshelves model.

--- app/models/bookshelf.rb
+++ app/models/bookshelf.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: bookshelves
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  shelf_name :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Bookshelf < ApplicationRecord
+    validates :user_id, :shelf_name, presence: true
+
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127183753) do
+ActiveRecord::Schema.define(version: 20171127215930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20171127183753) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["title"], name: "index_books_on_title", unique: true
+  end
+
+  create_table "bookshelves", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "shelf_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookshelves_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
