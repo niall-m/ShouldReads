44b37e963fdde12d651d809befe4418b0674b34a
Niall Mahford <niall.mahford@gmail.com>
2017-11-28 12:04:19 -0800

    Edit Books Reducer.

--- frontend/reducers/books_reducer.js
+++ frontend/reducers/books_reducer.js
@@ -1,13 +1,15 @@
+import merge from 'lodash/merge';
 import { RECEIVE_ALL_BOOKS,
          RECEIVE_BOOK 
 } from '../actions/book_actions';
-import merge from 'lodash/merge';
 
 const BooksReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     switch (action.type) {
         case RECEIVE_ALL_BOOKS:
+            return merge({}, oldState, action.books);
         case RECEIVE_BOOK:
+            return merge({}, oldState, { [action.book.id]: action.book });
         default:
             return oldState;
     }
