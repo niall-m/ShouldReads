54fb94f281e7be4efca5b460202a760d1c61ab32
Niall Mahford <niall.mahford@gmail.com>
2017-11-29 20:18:34 -0800

    Bookshelf controller, reconfigure controller rendering.

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,10 +1,12 @@
 class Api::BooksController < ApplicationController
   def index
     @books = Book.all
+    render :index
   end
 
   def show
     @book = Book.find(params[:id])
+    render :show
   end
 
   private
--- app/controllers/api/bookshelves_controller.rb
+++ app/controllers/api/bookshelves_controller.rb
@@ -1,16 +1,41 @@
 class Api::BookshelvesController < ApplicationController
   def index
+    @bookshelves = User.find(params[:user_id]).bookshelves
   end
 
   def show
+    user = User.find(params[:user_id])
+    @bookshelf = user.bookshelves.find_by(shelf_name: params[:shelf_name])
+    render :show
   end
 
   def create
+    @bookshelf = Bookshelf.new(bookshelf_params)
+    if @bookshelf.save
+      render :show
+    else
+      render json: @bookshelf.errors.full_messages, status: 422
+    end
   end
 
   def update
+    @bookshelf = Bookshelf.find(params[:id])
+    if @bookshelf.update_attributes(bookshelf_params)
+      render :show
+    else
+      render json: @bookshelf.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @bookshelf = Bookshelf.find(params[:id])
+    @bookshelf.destroy!
+    render :show
+  end
+
+  private
+  def bookshelf_params
+    params.require(:bookshelf).permit(:shelf_name)
   end
 end
+ 
