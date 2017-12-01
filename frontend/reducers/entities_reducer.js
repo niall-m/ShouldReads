c1918d8d32c972dfeeb7e7e5e8e6a75988d5e121
Niall Mahford <niall.mahford@gmail.com>
2017-12-01 10:37:47 -0800

    Create reviews reducer, import to entities reducer.

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import booksReducer from './books_reducer';
 import bookshelvesReducer from './bookshelves_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const entitiesReducer = combineReducers({
     books: booksReducer,
     bookshelves: bookshelvesReducer,
+    reviews: reviewsReducer,
 });
 
 export default entitiesReducer;
