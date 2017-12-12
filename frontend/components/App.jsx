19d5a33487e2e7e0d05ec25e8b92d17ad21f6382
Niall Mahford <niall.mahford@gmail.com>
2017-12-12 09:54:41 -0800

    Syntax change, modo change.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBarContainer from './nav_bar/nav_bar_container';
 import Splash from './splash/splash';
 import SignupContainer from './session/signup_container';
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default () => (
     <div className="splash-background">
         <div className="splash-container">
-            <h1 className="catchphrase">Meet your next<br />favorite book.</h1>
+            <h1 className="catchphrase">Find your next<br />favorite book.</h1>
             <div className="about-container">
                 <div className="about-catalogue">
                     <div className="section-header">
                     </div>
-                    <p>Peruse the catalogue of world famous books!</p>
+                    <p>Peruse the catalogue of books.</p>
                 </div>
                 <div className="about-tracking">
                     <div className="section-header">
