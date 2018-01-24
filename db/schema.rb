f122e419ca2256cd659b4124db8377c3cd702a00
Niall Mahford <niall.mahford@gmail.com>
2018-01-24 11:08:56 -0800

    Add rating column to reviews table with not null constraint. Had to drop reviews table to migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127233045) do
+ActiveRecord::Schema.define(version: 20180124183430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20171127233045) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
