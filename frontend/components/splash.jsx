d8c084f2b896d0f66674cf59d66f597904281fc8
Niall Mahford <niall.mahford@gmail.com>
2018-05-07 17:36:02 -0700

    Create splash GIFs and upload to s3.

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -14,5 +14,16 @@ export default () => (
                 <p>Rate your reads.</p>
             </div>
         </div>
+        <div className="gifs">
+            <div>
+                <img src="//drhktpaw9j8bp.cloudfront.net/search.gif" />
+            </div>
+            <div>
+                <img src="//drhktpaw9j8bp.cloudfront.net/dnd.gif" />
+            </div>
+            <div>
+                <img src="//drhktpaw9j8bp.cloudfront.net/rate.gif" />
+            </div>
+        </div>
     </section>
 );
