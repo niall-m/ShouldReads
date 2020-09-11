f0c7b3c3afb15c079d281a46636e636f77e57138
Niall Mahford <niall.mahford@gmail.com>
2020-09-11 16:55:41 -0700

    update dnd configuration

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { DragDropContext } from 'react-dnd';
+import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Favicon from 'react-favicon';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils';
@@ -14,18 +14,21 @@ import BookshelfShowContainer from './bookshelves/show/bookshelf_show_container'
 class App extends React.Component {
     render() {
         return (
-            <div id="app">
-                <Favicon url="//drhktpaw9j8bp.cloudfront.net/favicon-book.ico" />
-                <Route path="/" component={NavBarContainer} />
-                <ProtectedRoute path="/" component={BookshelfIndexContainer} />
-                <Switch>
-                    <AuthRoute exact path="/" component={Splash} />
-                    <ProtectedRoute path="/books/:bookId" component={BookShowContainer} />
-                    <ProtectedRoute path="/books" component={BookIndexContainer} />
-                    <ProtectedRoute path="/bookshelves/:bookshelfId" component={BookshelfShowContainer} />
-                </Switch>
-            </div>
+            <DndProvider backend={HTML5Backend}>
+                <div id="app">
+                    <Favicon url="//drhktpaw9j8bp.cloudfront.net/favicon-book.ico" />
+                    <Route path="/" component={NavBarContainer} />
+                    <ProtectedRoute path="/" component={BookshelfIndexContainer} />
+                    <Switch>
+                        <AuthRoute exact path="/" component={Splash} />
+                        <ProtectedRoute path="/books/:bookId" component={BookShowContainer} />
+                        <ProtectedRoute path="/books" component={BookIndexContainer} />
+                        <ProtectedRoute path="/bookshelves/:bookshelfId" component={BookshelfShowContainer} />
+                    </Switch>
+                </div>
+            </DndProvider>
         );
     }
 }
-export default DragDropContext(HTML5Backend)(App);
+
+export default App;
